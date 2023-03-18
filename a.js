277ea282ff5310dc655fbb2e1e77adf1f6999303
itomiya114 <116330970+itomiya114@users.noreply.github.com>
2023-03-18 18:27:23 +0900

    Update a.js

--- a.js
+++ a.js
@@ -2,3 +2,10 @@ setInterval(()=>{
 let amount=Game.computedMouseCps*1000;
 Game.Earn(amount);
 Game.handmadeCookies+=amount;},1000);
+Game.lumps=1e+200;Game.lumpsTotal=1e+200;
+setInterval(()=>{
+new Game.shimmer("golden");
+  new Game.shimmer("reindeer");
+
+}
+  ,30000);
